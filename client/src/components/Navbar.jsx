51056669c0f2cec391b0c436b26dd3225aca7f2d
Abhay-Prajapati54 <abhayprajapati9654@gmail.com>
2026-08-25 16:54:47 +0530

    add responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
+import logo from "../assets/logonav.png";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-logo">DGC Chakshu</Link>
+      <Link to="/" className="navbar-logo"><img src={logo} alt="" /></Link>
+
       <div className="navbar-links">
         <Link to="/blocks">Blocks</Link>
         <Link to="/departments">Departments</Link>
         <Link to="/faculty">Faculty</Link>
         <Link to="/search">Search</Link>
       </div>
+      <button class="menu-icon">
+        ☰
+      </button>
+
     </nav>
   );
 }
